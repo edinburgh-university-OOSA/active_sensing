5a3c641125b58b75f8c11a61d22f71a723dcccb2
sthancock <steven.hancock@ed.ac.uk>
2020-11-03 23:39:05 +0000

    Plotting script completed.

--- plot_gedi/plotComparison.py
+++ plot_gedi/plotComparison.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     '''
     p = argparse.ArgumentParser(description=("Writes out properties of GEDI waveform files"))
     p.add_argument("--real",dest="realName",type=str,help=("Input GEDI HDF5 filename"))
-    p.add_argument("--sim", dest ="simname", type=str,help=("Input simulated GEDI HDF5 filename"))
+    p.add_argument("--sim", dest ="simName", type=str,help=("Input simulated GEDI HDF5 filename"))
     p.add_argument("--outRoot",dest="outRoot",type=str,default='test',help=("Output graph filename root"))
     p.add_argument("--metric",dest="metName", type=str, default=" ", help=("Optional: Metric filename, for ground esoimates"))
     p.add_argument("--datumDiff",dest="datum", type=float, default=0.0, help=("Optional: Datum difference"))
@@ -49,10 +49,11 @@ class gediMetrics():
     '''Initialiser'''
     if(filename!=" "):
       self.waveID=np.genfromtxt(filename,usecols=(0,), unpack=True, dtype=str,comments='#')
-      self.ZG,self.cov,self.slope,self.sense=np.loadtxt(filename,usecols=(1,4,3,112), unpack=True, dtype=float,comments='#')
+      self.ZG,self.sense=np.loadtxt(filename,usecols=(5,112), unpack=True, dtype=float,comments='#')
       self.shotN=np.empty(self.waveID.shape,dtype=int)
       for i in range(0,self.waveID.shape[0]):
         self.shotN[i]=int(self.waveID[i].split('.')[2])
+      self.useMet=1   # use it
     else:
       self.useMet=0   # file is empty. Don't use
 
@@ -78,7 +79,11 @@ def plotComparison(gedi,sim,met,outRoot,datum=0):
       continue
 
     if(met.useMet==1):
-      metInd=np.where(met.shotN==shotN)[0][0]
+      metInd=np.where(met.shotN==shotN)
+      if(len(metInd)>0):
+        metInd=metInd[0]
+      else:
+        continue
 
     # make a graph
     output=outRoot+".beam."+str(shotN)+".png"
@@ -105,6 +110,8 @@ def plotComparison(gedi,sim,met,outRoot,datum=0):
     plt.plot(gedi.wave[i][0:gedi.nBins],gedi.z+datum,label='GEDI')
     plt.plot(sim.wave[simInd][0:sim.nBins]*reflScale+meanN,sim.z,label='Sim')
     plt.plot(sim.gWave[simInd][0:sim.nBins]*reflScale+meanN,sim.z,label='ALS ground')
+    if(met.useMet==1):
+      plt.plot([minY,maxY], [met.ZG[metInd],met.ZG[metInd]], color='b', linestyle='-', linewidth=2)
     plt.legend()
     plt.savefig(output)
     plt.close()
@@ -120,14 +127,11 @@ if __name__ == '__main__':
   # read the command line
   cmd=readCommands()
 
-  realName='/Users/dill/data/teaching/nerc_cdt/space/gedi/subset.GEDI01_B_2019223180803_O03756_T03613_02_003_01.h5'
-  simName='/Users/dill/data/teaching/nerc_cdt/space/gedi/sim.GEDI01_B_2019223180803_O03756_T03613_02_003_01.h5'
-
   # read real GEDI data
-  gedi=gediData(filename=realName)
+  gedi=gediData(filename=cmd.realName)
 
   # read simulated GEDI data
-  sim=gediData(filename=simName)
+  sim=gediData(filename=cmd.simName)
 
   # read metrics, if needed
   met=gediMetrics(cmd.metName)
