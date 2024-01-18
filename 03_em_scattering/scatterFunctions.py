9a179074a58d0a92a503cae15cb2b28e39010d3d
sthancock <steven.hancock@ed.ac.uk>
2024-01-18 17:24:43 +0000

    More work on week 3.

--- 03_em_scattering/scatterFunctions.py
+++ 03_em_scattering/scatterFunctions.py
@@ -31,7 +31,34 @@ class scatterDemo():
 
   ###########################
 
-  def makeSurface(rough=0.1,length=20.0):
+  def makeSurface(self,rough=0.1,length=20.0,res=0.01):
     '''Make a surface'''
 
+    # make sure that the roughness is well sampled
+    if(res>(rough/10.0)):
+      res=rough/10.0
+
+    # save variables
+    self.length=length
+    self.rough=rough
+    self.res=res
+
+    # allocate array
+    self.nBins=int(self.length/self.res)
+    self.x=np.linspace(0,self.length,num=self.nBins)
+    self.y=np.random.normal(loc=0.0,scale=self.rough,size=self.nBins) # make the random heights
     return
+
+  ###########################
+
+  def plotSurface(self):
+    '''Plot a surface'''
+    plt.xlabel("Distance (m)")
+    plt.ylabel("Height (m)")
+    plt.plot(self.x,self.y)
+    plt.show()
+    return
+
+
+#################################################
+
