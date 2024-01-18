28acc90be2981edc0efd4594f803f35bae439a77
sthancock <steven.hancock@ed.ac.uk>
2024-01-18 16:50:00 +0000

    Work started on Week 3 practical.

---
+++ 03_em_scattering/scatterFunctions.py
@@ -0,0 +1,37 @@
+'''
+A set of functions to demonstrate
+EM radiation scattering.
+
+University of Edinburgh
+Active Remote Sensing course
+
+Steven Hancock      2024
+svenhancock@gmail.com
+'''
+
+
+#################################################
+# import needed parts
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#################################################
+
+class scatterDemo():
+  '''Class to hold scatter demonstration
+     data and functions'''
+
+  ############################
+
+  def __init__(self):
+    # do nothing
+    return
+
+  ###########################
+
+  def makeSurface(rough=0.1,length=20.0):
+    '''Make a surface'''
+
+    return
