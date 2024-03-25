8c21854362f29ca988fb58b35eea99ed62e23073
sthancock <steven.hancock@ed.ac.uk>
2024-03-25 16:42:33 +0000

    Filtering script added.

---
+++ 11_change/gee_02_filter.js
@@ -0,0 +1,17 @@
+// A script to threshold and plot a DTM
+
+// Instantiate an image with the Image constructor.
+var image = ee.Image('CGIAR/SRTM90_V4');  // read SRTM
+
+var thresh=914;                           // threshold
+var dtm = image.select(['elevation']);    // read the elevation layer
+var subset1 = image.updateMask(dtm.gte(thresh)); // subset that is greater than or equal to thresh
+var subset2 = image.updateMask(dtm.lt(thresh));  // subset that is less than thresh
+
+// Zoom to a location.
+Map.setCenter( -3.9 ,56.8, 8); // Center on Scotland
+
+// Display the image on the map.
+Map.addLayer(subset2, {min: 0, max: 1345}, 'custom visualization');                   // less than thresh in greyscale
+Map.addLayer(subset1, {min: 0, max: 1345,palette: ['red']}, 'custom visualization');  // greater than thresh in red
+
