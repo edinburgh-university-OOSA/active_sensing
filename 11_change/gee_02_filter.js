02203f401f562025cff25764249d36ec930cf2db
sthancock <steven.hancock@ed.ac.uk>
2024-03-25 17:02:35 +0000

    JS 2 complete.

--- 11_change/gee_02_filter.js
+++ 11_change/gee_02_filter.js
@@ -1,17 +1,16 @@
-// A script to threshold and plot a DTM
+// A script to threshold and plot a DEM
 
-// Instantiate an image with the Image constructor.
 var image = ee.Image('CGIAR/SRTM90_V4');  // read SRTM
 
+// Threshold the DEM
 var thresh=914;                           // threshold
 var dem = image.select(['elevation']);    // read the elevation layer
 var subset1 = image.updateMask(dem.gte(thresh)); // subset that is greater than or equal to thresh
-var subset2 = image.updateMask(dem.lt(thresh));  // subset that is less than thresh
 
-// Zoom to a location.
-Map.setCenter( -3.9 ,56.8, 8); // Center on Scotland
+// Center on Scotland
+Map.setCenter( -3.9 ,56.8, 8);
 
 // Display the image on the map.
-Map.addLayer(subset2, {min: 0, max: 1345}, 'custom visualization');                   // less than thresh in greyscale
-Map.addLayer(subset1, {min: 0, max: 1345,palette: ['red']}, 'custom visualization');  // greater than thresh in red
+Map.addLayer(image, {min: 0, max: 1345}, 'SRTM elevation (m)');
+Map.addLayer(subset1, {min: 0, max: 1345,palette: ['red']}, 'SRTM over 914 m (m)');
 
