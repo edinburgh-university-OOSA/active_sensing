d820ea5fc120a84c8bfbb584ffab8e6d09454a1b
sthancock <steven.hancock@ed.ac.uk>
2024-03-25 18:09:05 +0000

    Up to task 5 complete.

--- 11_change/gee_04_s1change.js
+++ 11_change/gee_04_s1change.js
@@ -1,26 +1,19 @@
 // A script to view a single Sentinel-1 scene
 
-// Open the Sentinel-1 collection for VH and VV
+// Open the Sentinel-1 collection for VH
 var imgVH = ee.ImageCollection('COPERNICUS/S1_GRD')
         .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
         .filter(ee.Filter.eq('instrumentMode', 'IW'))
         .select('VH');
 
-var imgVV = ee.ImageCollection('COPERNICUS/S1_GRD')
-        .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
-        .filter(ee.Filter.eq('instrumentMode', 'IW'))
-        .select('VV');
-
-
-// separate ascending and descending orbits
-var descVH = imgVH.filter(ee.Filter.eq('orbitProperties_pass', 'DESCENDING'));
+// separate ascending orbits
 var ascVH = imgVH.filter(ee.Filter.eq('orbitProperties_pass', 'ASCENDING'));
 
-// dates for two montsh before and after storm Arwen, skipping the say of the storm
+// dates for two months before and after storm Arwen, skipping the say of the storm
 var beforeDates = ee.Filter.date('2021-09-25', '2021-11-25');
 var afterDates = ee.Filter.date('2021-11-27', '2022-01-27');
 
-// take the mean backscatter
+// take the mean backscatter before and after the event
 var beforeVH = ascVH.filter(beforeDates).mean();
 var afterVH = ascVH.filter(afterDates).mean();
 
---
+++ 11_change/gee_05_allbands.js
@@ -0,0 +1,45 @@
+// A script to view a single Sentinel-1 scene
+
+// Open the Sentinel-1 collection for VH and VV
+var imgVH = ee.ImageCollection('COPERNICUS/S1_GRD')
+        .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
+        .filter(ee.Filter.eq('instrumentMode', 'IW'))
+        .select('VH');
+
+var imgVV = ee.ImageCollection('COPERNICUS/S1_GRD')
+        .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
+        .filter(ee.Filter.eq('instrumentMode', 'IW'))
+        .select('VV');
+
+// separate ascending and descending orbits
+var descVH = imgVH.filter(ee.Filter.eq('orbitProperties_pass', 'DESCENDING'));
+var ascVH = imgVH.filter(ee.Filter.eq('orbitProperties_pass', 'ASCENDING'));
+var descVV = imgVV.filter(ee.Filter.eq('orbitProperties_pass', 'DESCENDING'));
+var ascVV = imgVV.filter(ee.Filter.eq('orbitProperties_pass', 'ASCENDING'));
+
+// dates for two months before and after storm Arwen, skipping the say of the storm
+var beforeDates = ee.Filter.date('2021-09-25', '2021-11-25');
+var afterDates = ee.Filter.date('2021-11-27', '2022-01-27');
+
+// take the mean backscatter
+var beforeVHasc = ascVH.filter(beforeDates).mean();
+var afterVHasc = ascVH.filter(afterDates).mean();
+var beforeVHdesc = descVH.filter(beforeDates).mean();
+var afterVHdesc = descVH.filter(afterDates).mean();
+var beforeVVasc = ascVV.filter(beforeDates).mean();
+var afterVVasc = ascVV.filter(afterDates).mean();
+var beforeVVdesc = descVV.filter(beforeDates).mean();
+var afterVVdesc = descVV.filter(afterDates).mean();
+
+// make the map
+Map.setCenter( -2.330734 ,57.025515, 13);
+Map.addLayer(beforeVHasc, {min: -25, max: 5}, 'VH ascending before', true);
+Map.addLayer(afterVHasc, {min: -25, max: 5}, 'VH ascending after', true);
+Map.addLayer(beforeVHdesc, {min: -25, max: 5}, 'VH descending before', true);
+Map.addLayer(afterVHdesc, {min: -25, max: 5}, 'VH descending after', true);
+Map.addLayer(beforeVVasc, {min: -25, max: 5}, 'VV ascending before', true);
+Map.addLayer(afterVVasc, {min: -25, max: 5}, 'VV ascending after', true);
+Map.addLayer(beforeVVdesc, {min: -25, max: 5}, 'VV descending before', true);
+Map.addLayer(afterVVdesc, {min: -25, max: 5}, 'VV descending after', true);
+
+
